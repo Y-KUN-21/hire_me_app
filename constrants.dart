4a0fe3ffa9592b2466cbec7086090faa5b9ba9e9
Y-KUN-21 <57254049+Y-KUN-21@users.noreply.github.com>
2020-10-01 20:31:50 +0530

    Delete constrants.dart

--- constrants.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-Color backgroundcolor = Color(0xFFF1F2F6);
-Color shadowcolor = Color(0xFFDADFF0);
-Color textcolor = Color(0xFF707070);
-Color selectedLabelText = Color(0xff0c18fb);
-Color unselectedLabelText = Colors.grey;
