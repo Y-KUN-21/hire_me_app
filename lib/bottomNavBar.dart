8bc9d7dcfe8d6c8b6a20baab600377313ec4eb3a
Y-KUN-21 <57254049+Y-KUN-21@users.noreply.github.com>
2020-10-06 20:27:16 +0530

    Add files via upload

--- lib/bottomNavBar.dart
+++ lib/bottomNavBar.dart
@@ -25,37 +25,33 @@ class _BottomNavBarState extends State<BottomNavBar> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: _screens[_currentIndex],
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.only(bottom: 8.0),
-        child: CustomNavigationBar(
-          iconSize: 30.0,
-          selectedColor: selectedLabelText,
-          strokeColor: Color(0x300c18fb),
-          unSelectedColor: unselectedLabelText,
-          backgroundColor: backgroundcolor,
-          borderRadius: Radius.circular(15.0),
-          items: [
-            CustomNavigationBarItem(
-              icon: EvaIcons.homeOutline,
-            ),
-            CustomNavigationBarItem(
-              icon: EvaIcons.browserOutline,
-            ),
-            CustomNavigationBarItem(
-              icon: EvaIcons.searchOutline,
-            ),
-            CustomNavigationBarItem(
-              icon: EvaIcons.peopleOutline,
-            ),
-          ],
-          currentIndex: _currentIndex,
-          onTap: (index) {
-            setState(() {
-              _currentIndex = index;
-            });
-          },
-          isFloating: true,
-        ),
+      bottomNavigationBar: CustomNavigationBar(
+        iconSize: 30.0,
+        selectedColor: selectedLabelText,
+        strokeColor: Color(0x300c18fb),
+        unSelectedColor: unselectedLabelText,
+        backgroundColor: backgroundcolor,
+        borderRadius: Radius.circular(15.0),
+        items: [
+          CustomNavigationBarItem(
+            icon: EvaIcons.homeOutline,
+          ),
+          CustomNavigationBarItem(
+            icon: EvaIcons.browserOutline,
+          ),
+          CustomNavigationBarItem(
+            icon: EvaIcons.searchOutline,
+          ),
+          CustomNavigationBarItem(
+            icon: EvaIcons.peopleOutline,
+          ),
+        ],
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
       ),
     );
   }
